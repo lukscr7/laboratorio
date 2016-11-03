19772075031096370c06f467d86248aad4c8ea35
LucasDiaz <lukcr7@gmail.com>
2016-11-02 23:59:10 -0300

    estilos

--- app/controller/login.php
+++ app/controller/login.php
@@ -13,7 +13,7 @@ class Login_Controller{
     {
         $usuario = new Usuario();
         echo 'hola';
-        //   Si el usuario ya pasó por la pantalla inicial y presionó el botón "Ingresar"
+        // Si el usuario ya pasó por la pantalla inicial y presionó el botón "Ingresar"
         $existeUsuario= $usuario->dat_usuario($_POST['usuario_loguin']);
         if($existeUsuario){   // La variable $ExisteUsuarioyPassoword recibe valor TRUE si el usuario existe y FALSE en caso que no. Este valor lo determina el modelo.
                     $validar = new Validacion();

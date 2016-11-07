e204c600283f2f72f0d7b8d9a91a52f43638f7ba
DarioExequielFlores <darioexequiel22@gmail.com>
2016-11-07 10:34:47 -0300

    ayudando a lucas nº 55

--- app/controller/login.php
+++ app/controller/login.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: LuksCR7
@@ -6,66 +7,71 @@
  * Time: 18:03
  *
  */
+class Login_Controller
+{
 
-class Login_Controller{
-
-    function iniciarSesion(){
+    function iniciarSesion()
+    {
         $us = new Usuario();
         $usuario = $_POST["usuario_loguin"];
         $pass = $_POST["pass_loguin"];
-        if ($us->verificarUsuario($usuario,$pass)){
+        if ($us->verificarUsuario($usuario, $pass)) {
             $us->dat_usuario($usuario);
             $_SESSION["usuario"] = $us->getUserId();
             $_SESSION["permiso"] = $us->getPermisos();
             header('Location: index.php');
-        }else{
+        } else {
             header('Location: index.php?notLog=UsuarioMal#log-in'); //#log-in
         }
     }
 
-    public function cerrarSesion(){
+    public function cerrarSesion()
+    {
         session_destroy();
         header('Location: index.php');
     }
 
 
-
     //public
-    public function registrarUsuario(){//hecho asi nomas.. ya lo modifico..... ahora CALCULO AVANZADO
-        $us= new Usuario();
-
+    public function registrarUsuario()
+    {//hecho asi nomas.. ya lo modifico..... ahora CALCULO AVANZADO
+        $us = new Usuario();
+        $vacio = false;
         foreach ($_POST as $key => $value) {
-            if ($key <> 'correo_perfil' ) {
-                $validar = new Validacion();
-                global $baseDatos;
-                $usuario = $baseDatos->real_escape_string($_POST['usuario_perfil']);
-                $pass = $baseDatos->real_escape_string($_POST['pass_perfil']);
-                $pass2 = $baseDatos->real_escape_string($_POST['pass2_perfil']);
-                $nombre = $baseDatos->real_escape_string($_POST['nombre_perfil']);
-                $apellido = $baseDatos->real_escape_string($_POST['apellido_perfil']);
-                $correo = $baseDatos->real_escape_string($_POST['correo_perfil']);
+            if (empty($value)) {
+                $vacio = true;
+            }
+        }
+        if (!$vacio) {
+            $validar = new Validacion();
+            global $baseDatos;
+            $usuario = $baseDatos->real_escape_string($_POST['usuario_perfil']);
+            $pass = $baseDatos->real_escape_string($_POST['pass_perfil']);
+            $pass2 = $baseDatos->real_escape_string($_POST['pass2_perfil']);
+            $nombre = $baseDatos->real_escape_string($_POST['nombre_perfil']);
+            $apellido = $baseDatos->real_escape_string($_POST['apellido_perfil']);
+            $correo = $baseDatos->real_escape_string($_POST['correo_perfil']);
 
-                $verificarExistencia = $us->existeUsuario($usuario);
-                $verificarPass = $validar->pass($pass, $pass2);
-                if ($verificarExistencia == false) {
-                    if ($verificarPass == true) {
-                        $verificarInsert = $us->insert($usuario, $pass, $nombre, $apellido, $correo);
-                        if ($verificarInsert == true) {
-                            header('Location: index.php?notReg=Verificar#Registro');
-                        }
-
+            $verificarExistencia = $us->existeUsuarioID($usuario);
+            $verificarPass = $validar->pass($pass, $pass2);
+            if ($verificarExistencia == false) {
+                if ($verificarPass == true) {
+                    $verificarInsert = $us->insert($usuario, $pass, $nombre, $apellido, $correo);
+                    if ($verificarInsert == true) {
+                        header('Location: index.php?notReg=Verificar#Registro');
                     } else {
-                        header('Location: index.php?notReg=Pass#Registro');
+                        header('Location: index.php?notReg=ErrorInsert#Registro');
                     }
+
                 } else {
-                    header('Location: index.php?notReg=Existe#Registro');
+                    header('Location: index.php?notReg=Pass#Registro');
                 }
             } else {
-                header('Location: index.php?notReg=SinDatos#Registro');
+                header('Location: index.php?notReg=Existe#Registro');
             }
+        }else{
+            header('Location: index.php?notReg=SinDatos#Registro');
         }
 
     }
-
-
 }

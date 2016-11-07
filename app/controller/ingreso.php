71baf45fffdf04a287623692f0c9eb67cc73c97b
LucasDiaz <lukcr7@gmail.com>
2016-11-07 06:45:24 -0300

    conflicto 1 resuelto

--- app/controller/ingreso.php
+++ app/controller/ingreso.php
@@ -10,20 +10,19 @@ class Ingreso_Controller{
     public static function main(){
 
         if (isset($_SESSION["usuario"])){
-<<<<<<< HEAD
+
             if ($_SESSION["permiso"] == "CLIENTE"){
-                $perfil = new Perfil_Controller();
-                $webapp = $perfil->cliente();
+                $reserva = new Reserva_Controller();
+                $webapp = $reserva->formReserva();
             }
             if ($_SESSION["permiso"] == "ADMIN"){
                 $admin = new Administrador_Controller();
                 $webapp = $admin->admin();
             }
 
-=======
-            $reserva = new Reserva_Controller();
-            $webapp = $reserva->formReserva();
->>>>>>> origin/master
+
+
+
         }else{
             $tpl = new TemplatePower("template/login.html");
             $tpl->prepare();
